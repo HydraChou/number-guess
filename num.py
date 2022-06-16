5168d49937e33d80956e08387585cdcf1c6a2992
Athena1023 <cychou@g.nccu.edu.tw>
2022-06-16 17:22:54 +0800

    第二版

--- num.py
+++ num.py
@@ -1,14 +1,17 @@
 import random
 
 r = random.randint (1, 100) # random是函式
-
+count = 0
 while True:
-    num = input ('請輸入一個1-100之間的整數: ') #input會把所有東西存成字串
-    num = int(num) #型別轉換
+	count = count + 1 #等同於 count += 1
+    num = input ('請輸入一個1-100之間的整數:')
+    num = int (num) #型別轉換
     if num < r:
       print ('數字太小了喔')
     elif num > r:
       print ('數字超過了啦')
     else: 
       print ('終於猜對了')
+      print ('這是你猜的第', count, '次') 
       break
+    print ('這是你猜的第', count, '次')
